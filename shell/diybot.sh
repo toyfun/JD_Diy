3a4f090f0b8d5bd0762fcfa3782b10bfa64686c8
toyfun <247037681@qq.com>
2022-04-10 18:36:21 +0800

    Update diybot.sh

--- shell/diybot.sh
+++ shell/diybot.sh
@@ -6,7 +6,7 @@ else
   root=/ql
 fi
 
-diybot_url="https://github.com/qirenzhidao/JD_Diy.git"
+diybot_url="https://github.com/toyfun/JD_Diy.git"
 jbot_botset=$root/config/bot.json
 diybot_repo=$root/repo/diybot
 diybot_diy=$root/jbot/diy
